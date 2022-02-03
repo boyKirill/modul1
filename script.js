f909e2f46c22b806b81d33e642dfe082818e74db
Kirill Vladimirov <k.vladimirov@yandex.ru>
2022-02-03 19:17:16 +0300

    Добавление бургера

--- script.js
+++ script.js
@@ -33,6 +33,19 @@ if (isMobile.any()) {
 	document.body.classList.add('_pc');
 }
 
+//меню бургер
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu) {
+
+	iconMenu.addEventListener("click", function (e) {
+		document.body.classList.toggle('_lock');
+		iconMenu.classList.toggle('_active');
+		menuBody.classList.toggle('_active');
+	})
+}
+
+
 
 const scrollBtns = document.querySelectorAll('.scrollBtn[data-goto]');
 if (scrollBtns.length > 0) {
@@ -45,6 +58,12 @@ if (scrollBtns.length > 0) {
 		if (scrollBtn.dataset.goto && document.querySelector(scrollBtn.dataset.goto)) {
 			const gotoBlock = document.querySelector(scrollBtn.dataset.goto);
 			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset;
+
+			if (iconMenu.classList.contains('_active')) {
+				document.body.classList.remove('_lock');
+				iconMenu.classList.remove('_active');
+				menuBody.classList.remove('_active');
+			}
 
 			window.scrollTo({
 				top: gotoBlockValue,
